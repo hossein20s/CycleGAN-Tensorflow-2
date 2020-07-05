5cd63d86df1f674f2c5dbfa51b4f40e3e6b59522
Hossein Aklaghpour <hossein.akhlaghpour@auto-grid.com>
2020-07-05 04:16:31 +0000

    with 2 epichs finished

--- train.py
+++ train.py
@@ -14,72 +14,6 @@ import module
 
 
 # ==============================================================================
-# =                                   param                                    =
-# ==============================================================================
-if __name__ == '__main__':
-    py.arg('--image_ext', default='jpg') # gif
-    py.arg('--dataset', default='summer2winter_yosemite') # horse2zebra
-    py.arg('--datasets_dir', default='datasets')
-    py.arg('--load_size', type=int, default=286)  # load image to this size
-    py.arg('--crop_size', type=int, default=256)  # then crop to this size
-    py.arg('--batch_size', type=int, default=1)
-    py.arg('--epochs', type=int, default=200)
-    py.arg('--epoch_decay', type=int, default=100)  # epoch to start decaying learning rate
-    py.arg('--lr', type=float, default=0.0002)
-    py.arg('--beta_1', type=float, default=0.5)
-    py.arg('--adversarial_loss_mode', default='lsgan', choices=['gan', 'hinge_v1', 'hinge_v2', 'lsgan', 'wgan'])
-    py.arg('--gradient_penalty_mode', default='none', choices=['none', 'dragan', 'wgan-gp'])
-    py.arg('--gradient_penalty_weight', type=float, default=10.0)
-    py.arg('--cycle_loss_weight', type=float, default=10.0)
-    py.arg('--identity_loss_weight', type=float, default=0.0)
-    py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
-    args = py.args()
-
-    # output_dir
-    output_dir = py.join('output', args.dataset)
-    py.mkdir(output_dir)
-
-    # save settings
-    py.args_to_yaml(py.join(output_dir, 'settings.yml'), args)
-
-
-    # ==============================================================================
-    # =                                    data                                    =
-    # ==============================================================================
-
-    A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.{}'.format(args.image_ext))
-    B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.{}'.format(args.image_ext))
-    A_B_dataset, len_dataset = data.make_zip_dataset(A_img_paths, B_img_paths, args.batch_size, args.load_size, args.crop_size, training=True, repeat=False)
-
-    A2B_pool = data.ItemPool(args.pool_size)
-    B2A_pool = data.ItemPool(args.pool_size)
-
-    A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.{}'.format(args.image_ext))
-    B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.{}'.format(args.image_ext))
-    A_B_dataset_test, _ = data.make_zip_dataset(A_img_paths_test, B_img_paths_test, args.batch_size, args.load_size, args.crop_size, training=False, repeat=True)
-
-
-    # ==============================================================================
-    # =                                   models                                   =
-    # ==============================================================================
-
-    G_A2B = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3))
-    G_B2A = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3))
-
-    D_A = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
-    D_B = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
-
-    d_loss_fn, g_loss_fn = gan.get_adversarial_losses_fn(args.adversarial_loss_mode)
-    cycle_loss_fn = tf.losses.MeanAbsoluteError()
-    identity_loss_fn = tf.losses.MeanAbsoluteError()
-
-    G_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
-    D_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
-    G_optimizer = keras.optimizers.Adam(learning_rate=G_lr_scheduler, beta_1=args.beta_1)
-    D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.beta_1)
-
-
-# ==============================================================================
 # =                                 train step                                 =
 # ==============================================================================
 
@@ -103,7 +37,8 @@ def train_G(A, B):
         A2A_id_loss = identity_loss_fn(A, A2A)
         B2B_id_loss = identity_loss_fn(B, B2B)
 
-        G_loss = (A2B_g_loss + B2A_g_loss) + (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight + (A2A_id_loss + B2B_id_loss) * args.identity_loss_weight
+        G_loss = (A2B_g_loss + B2A_g_loss) + (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight + (
+                    A2A_id_loss + B2B_id_loss) * args.identity_loss_weight
 
     G_grad = t.gradient(G_loss, G_A2B.trainable_variables + G_B2A.trainable_variables)
     G_optimizer.apply_gradients(zip(G_grad, G_A2B.trainable_variables + G_B2A.trainable_variables))
@@ -162,59 +97,125 @@ def sample(A, B):
 
 
 # ==============================================================================
-# =                                    run                                     =
+# =                                   param                                    =
 # ==============================================================================
+if __name__ == '__main__':
+    py.arg('--image_ext', default='jpg')  # gif
+    py.arg('--dataset', default='summer2winter_yosemite')  # horse2zebra
+    py.arg('--datasets_dir', default='datasets')
+    py.arg('--load_size', type=int, default=286)  # load image to this size
+    py.arg('--crop_size', type=int, default=256)  # then crop to this size
+    py.arg('--batch_size', type=int, default=1)
+    py.arg('--epochs', type=int, default=200)
+    py.arg('--epoch_decay', type=int, default=100)  # epoch to start decaying learning rate
+    py.arg('--lr', type=float, default=0.0002)
+    py.arg('--beta_1', type=float, default=0.5)
+    py.arg('--adversarial_loss_mode', default='lsgan', choices=['gan', 'hinge_v1', 'hinge_v2', 'lsgan', 'wgan'])
+    py.arg('--gradient_penalty_mode', default='none', choices=['none', 'dragan', 'wgan-gp'])
+    py.arg('--gradient_penalty_weight', type=float, default=10.0)
+    py.arg('--cycle_loss_weight', type=float, default=10.0)
+    py.arg('--identity_loss_weight', type=float, default=0.0)
+    py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
+    args = py.args()
 
-# epoch counter
-ep_cnt = tf.Variable(initial_value=0, trainable=False, dtype=tf.int64)
+    # output_dir
+    output_dir = py.join('output', args.dataset)
+    py.mkdir(output_dir)
 
-# checkpoint
-checkpoint = tl.Checkpoint(dict(G_A2B=G_A2B,
-                                G_B2A=G_B2A,
-                                D_A=D_A,
-                                D_B=D_B,
-                                G_optimizer=G_optimizer,
-                                D_optimizer=D_optimizer,
-                                ep_cnt=ep_cnt),
-                           py.join(output_dir, 'checkpoints'),
-                           max_to_keep=5)
-try:  # restore checkpoint including the epoch counter
-    checkpoint.restore().assert_existing_objects_matched()
-except Exception as e:
-    print(e)
+    # save settings
+    py.args_to_yaml(py.join(output_dir, 'settings.yml'), args)
 
-# summary
-train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'train'))
+    # ==============================================================================
+    # =                                    data                                    =
+    # ==============================================================================
 
-# sample
-test_iter = iter(A_B_dataset_test)
-sample_dir = py.join(output_dir, 'samples_training')
-py.mkdir(sample_dir)
+    A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.{}'.format(args.image_ext))
+    B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.{}'.format(args.image_ext))
+    A_B_dataset, len_dataset = data.make_zip_dataset(A_img_paths, B_img_paths, args.batch_size, args.load_size,
+                                                     args.crop_size, training=True, repeat=False)
 
-# main loop
-with train_summary_writer.as_default():
-    for ep in tqdm.trange(args.epochs, desc='Epoch Loop'):
-        if ep < ep_cnt:
-            continue
+    A2B_pool = data.ItemPool(args.pool_size)
+    B2A_pool = data.ItemPool(args.pool_size)
 
-        # update epoch counter
-        ep_cnt.assign_add(1)
+    A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.{}'.format(args.image_ext))
+    B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.{}'.format(args.image_ext))
+    A_B_dataset_test, _ = data.make_zip_dataset(A_img_paths_test, B_img_paths_test, args.batch_size, args.load_size,
+                                                args.crop_size, training=False, repeat=True)
 
-        # train for an epoch
-        for A, B in tqdm.tqdm(A_B_dataset, desc='Inner Epoch Loop', total=len_dataset):
-            G_loss_dict, D_loss_dict = train_step(A, B)
+    # ==============================================================================
+    # =                                   models                                   =
+    # ==============================================================================
 
-            # # summary
-            tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
-            tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
-            tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations, name='learning rate')
+    G_A2B = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3))
+    G_B2A = module.ResnetGenerator(input_shape=(args.crop_size, args.crop_size, 3))
 
-            # sample
-            if G_optimizer.iterations.numpy() % 100 == 0:
-                A, B = next(test_iter)
-                A2B, B2A, A2B2A, B2A2B = sample(A, B)
-                img = im.immerge(np.concatenate([A, A2B, A2B2A, B, B2A, B2A2B], axis=0), n_rows=2)
-                im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))
+    D_A = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
+    D_B = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
 
-        # save checkpoint
-        checkpoint.save(ep)
+    d_loss_fn, g_loss_fn = gan.get_adversarial_losses_fn(args.adversarial_loss_mode)
+    cycle_loss_fn = tf.losses.MeanAbsoluteError()
+    identity_loss_fn = tf.losses.MeanAbsoluteError()
+
+    G_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
+    D_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
+    G_optimizer = keras.optimizers.Adam(learning_rate=G_lr_scheduler, beta_1=args.beta_1)
+    D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.beta_1)
+
+    # ==============================================================================
+    # =                                    run                                     =
+    # ==============================================================================
+
+    # epoch counter
+    ep_cnt = tf.Variable(initial_value=0, trainable=False, dtype=tf.int64)
+
+    # checkpoint
+    checkpoint = tl.Checkpoint(dict(G_A2B=G_A2B,
+                                    G_B2A=G_B2A,
+                                    D_A=D_A,
+                                    D_B=D_B,
+                                    G_optimizer=G_optimizer,
+                                    D_optimizer=D_optimizer,
+                                    ep_cnt=ep_cnt),
+                               py.join(output_dir, 'checkpoints'),
+                               max_to_keep=5)
+    try:  # restore checkpoint including the epoch counter
+        checkpoint.restore().assert_existing_objects_matched()
+    except Exception as e:
+        print(e)
+
+    # summary
+    train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'train'))
+
+    # sample
+    test_iter = iter(A_B_dataset_test)
+    sample_dir = py.join(output_dir, 'samples_training')
+    py.mkdir(sample_dir)
+
+    # main loop
+    with train_summary_writer.as_default():
+        for ep in tqdm.trange(args.epochs, desc='Epoch Loop'):
+            if ep < ep_cnt:
+                continue
+
+            # update epoch counter
+            ep_cnt.assign_add(1)
+
+            # train for an epoch
+            for A, B in tqdm.tqdm(A_B_dataset, desc='Inner Epoch Loop', total=len_dataset):
+                G_loss_dict, D_loss_dict = train_step(A, B)
+
+                # # summary
+                tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
+                tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
+                tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations,
+                           name='learning rate')
+
+                # sample
+                if G_optimizer.iterations.numpy() % 100 == 0:
+                    A, B = next(test_iter)
+                    A2B, B2A, A2B2A, B2A2B = sample(A, B)
+                    img = im.immerge(np.concatenate([A, A2B, A2B2A, B, B2A, B2A2B], axis=0), n_rows=2)
+                    im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))
+
+            # save checkpoint
+            checkpoint.save(ep)
